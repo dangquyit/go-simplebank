81761cac17c8fb434b15839703a2ee443b0dfd76
Nguyen Dang Quy <dangquyitt@gmail.com>
2023-03-15 23:42:11 +0700

    config make file and create migation

---
+++ Makefile
@@ -0,0 +1,11 @@
+postgres:
+	docker run --name postgres -p 5431:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=1234 -d postgres:latest
+createdb:
+	docker exec -it postgres createdb --username=root --owner=root simple_bank
+dropdb:
+	docker exec -it postgres dropdb simple_bank
+migrateup:
+	migrate -path db/migration -database "postgresql://root:1234@localhost:5431/simple_bank?sslmode=disable" -verbose up
+migratedown:
+	migrate -path db/migration -database "postgresql://root:1234@localhost:5431/simple_bank?sslmode=disable" -verbose down
+.PHONY: createdb dropdb postgres migrateup migratedown
---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,48 @@
+CREATE TABLE "accounts" (
+                            "id" BIGSERIAL PRIMARY KEY,
+                            "account_number" int UNIQUE NOT NULL,
+                            "owner" varchar NOT NULL,
+                            "user_name" varchar UNIQUE NOT NULL,
+                                "email" varchar UNIQUE NOT NULL,
+                            "password" varchar NOT NULL,
+                            "number" bigint UNIQUE NOT NULL,
+                            "balance" bigint NOT NULL,
+                            "currency" varchar NOT NULL,
+                            "created_at" timestamp NOT NULL DEFAULT (now()),
+                            "updated_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+                           "id" BIGSERIAL PRIMARY KEY,
+                           "account_number" bigint NOT NULL,
+                           "amount" bigint NOT NULL,
+                           "created_at" timestamp NOT NULL DEFAULT (now()),
+                           "updated_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+                             "id" BIGSERIAL PRIMARY KEY,
+                             "from_account_number" bigint NOT NULL,
+                             "to_account_number" bigint NOT NULL,
+                             "amount" bigint NOT NULL,
+                             "created_at" timestamp NOT NULL DEFAULT (now()),
+                             "updated_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_number");
+
+CREATE INDEX ON "transfers" ("to_account_number");
+
+CREATE INDEX ON "transfers" ("from_account_number");
+
+CREATE INDEX ON "transfers" ("from_account_number", "to_account_number");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_number") REFERENCES "accounts" ("account_number");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_number") REFERENCES "accounts" ("account_number");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_number") REFERENCES "accounts" ("account_number");

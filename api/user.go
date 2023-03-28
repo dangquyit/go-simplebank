32b9b5e7e746aa1fdebdfbc836773f1f2aeaae58
Nguyen Dang Quy <dangquyitt@gmail.com>
2023-03-28 23:43:07 +0700

    add API create user and hashing password

---
+++ api/user.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	db "github.com/dangquyit/go-simplebank/db/sqlc"
+	"github.com/dangquyit/go-simplebank/util"
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	"log"
+	"net/http"
+	"time"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	result, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			log.Println(pqErr.Code.Name())
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	rsp := createUserResponse{
+		Username:          result.Username,
+		FullName:          result.FullName,
+		Email:             result.Email,
+		PasswordChangedAt: result.PasswordChangedAt,
+		CreatedAt:         result.CreatedAt,
+		UpdatedAt:         result.UpdatedAt,
+	}
+	ctx.JSON(http.StatusOK, rsp)
+}

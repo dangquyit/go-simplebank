cea09ca20e2476f761a4dd715b1f0776c6b1f648
Nguyen Dang Quy <dangquyitt@gmail.com>
2023-04-02 15:04:50 +0700

    create jwt token and unit test

---
+++ token/payload.go
@@ -0,0 +1,44 @@
+package token
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+// Different types of error returned by the VerifyToken function
+var (
+	ErrExpiredToken = errors.New("token has expired")
+	ErrInvalidToken = errors.New("token is invalid")
+)
+
+// Payload contains the payload data of the token
+type Payload struct {
+	Id        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+func (p Payload) Valid() error {
+	if time.Now().After(p.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
+
+// NewPayload creates a new token payload with specific username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenId, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		Id:        tokenId,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
